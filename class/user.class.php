411032d375fbb52812743180ad955d89443c1451
Guillaume <grannaud@gmail.com>
2015-02-02 09:09:47 +0100

    js + css

--- class/user.class.php
+++ class/user.class.php
@@ -1,5 +1,24 @@
 <?php
-class user {
-	
+class User {
+
+	private $_firstname;
+	private $_lastname;
+	private $_picture;
+
+	public function updateFirstame() {
+
+	}
+
+	public function updateLastame() {
+
+	}
+
+	public function updatePicture() {
+
+	} 
+
+	public function createEvent() {
+
+	}
 }
 ?>
